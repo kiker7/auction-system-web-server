3f17c7d3d303a028586fad3d5b88dbcb8352484d
rrutyna <rafalrutyna00@gmai.com>
2018-11-15 11:58:06 +0100

    Test case for user operations

--- src/test/java/pl/edu/pw/ee/rutynar/auctionsystem/api/UserRouterTest.java
+++ src/test/java/pl/edu/pw/ee/rutynar/auctionsystem/api/UserRouterTest.java
@@ -7,17 +7,22 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import pl.edu.pw.ee.rutynar.auctionsystem.config.security.JwtTokenUtil;
 import pl.edu.pw.ee.rutynar.auctionsystem.data.domain.User;
 import pl.edu.pw.ee.rutynar.auctionsystem.data.repository.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.web.reactive.function.BodyInserters.fromFormData;
+import static org.springframework.web.reactive.function.BodyInserters.fromObject;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -30,15 +35,45 @@ class UserRouterTest {
     @Autowired
     private UserRepository userRepository;
 
-    private List<User> expectedList;
+    @Autowired
+    private JwtTokenUtil tokenUtil;
+
+    private String token;
+
+    private List<User> expectedList = new ArrayList<>();
 
     @BeforeEach
     void beforeEach() {
         this.expectedList = userRepository.findAll().collectList().block();
-        this.client = this.client.mutate().baseUrl("/user").build();
+        this.token = tokenUtil.generateToken(expectedList.size() > 0 ? expectedList.get(0) : null);
+
+        this.client = this.client.mutate()
+                .baseUrl("/api/user")
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                .build();
     }
 
-    /* -------------------- No security ------------------ */
+    @Test
+    void testAuthorizedAccess(){
+        client
+                .get()
+                .uri("/")
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void testInvalidToken(){
+        client
+                .get()
+                .uri("/")
+                .header("Authorization", "empty..")
+                .exchange()
+                .expectBody()
+                .jsonPath("$.message").isEqualTo("Invalid token...");
+    }
+
     @Test
     void testGetAllUsers() {
         client
@@ -52,13 +87,22 @@ class UserRouterTest {
     }
 
     @Test
-    void testUserInvalidNotFound() {
+    void testUserInvalidId() {
         client
                 .get()
-                .uri("/invalid")
+                .uri("/4a4a4a4a")
                 .exchange()
                 .expectStatus()
-                .isNotFound();
+                .isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Test
+    void testUserNotFound(){
+        client
+                .get()
+                .uri("5bec9d1a582eca0711956818")
+                .exchange()
+                .expectStatus().isNotFound();
     }
 
     @Test
@@ -74,16 +118,20 @@ class UserRouterTest {
                 .isEqualTo(expectedUser);
     }
 
-    /* -------------------------------------------------- */
-
     @Test
-    void testUnauthorizedAccess(){
+    void testUserUpdate(){
+        User user = expectedList.get(0);
+        // Change user data
+        user.setEmail("newEmail@tst.com");
+
         client
-                .get()
-                .uri("/api/user")
+                .put()
+                .uri("/{id}", user.getId())
+                .body(fromObject(user))
                 .exchange()
-                .expectStatus().isUnauthorized();
+                .expectBody(User.class)
+                .isEqualTo(user);
     }
 
-    //TODO: post, put, delete
+
 }
